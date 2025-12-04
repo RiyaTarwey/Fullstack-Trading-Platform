23c13f39914af16b4908da6bb801748a35a28773
Riya Tarwey <riyakumari9189@gmail.com>
2025-12-04 22:33:35 +0530

    Updated small bug

--- Frontend/src/page/Portfolio/Portfolio.jsx
+++ Frontend/src/page/Portfolio/Portfolio.jsx
@@ -47,7 +47,7 @@ const Portfolio = () => {
                 </TableCell>
                 <TableCell>{item.coin.symbol?.toUpperCase()}</TableCell>
                 <TableCell>{item.quantity}</TableCell>
-                <TableCell>{item.coin.price_change_price_24h}</TableCell>
+                <TableCell>{item.coin.price_change_24h}</TableCell>
                 <TableCell>{item.coin.price_change_percentage_24h}</TableCell>
                 <TableCell className="text-right">
                   {item.coin.total_volume}
